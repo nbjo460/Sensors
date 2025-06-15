4d9e3384d1bf866410e8a4e273b17301c2ec6be3
menachem yarhi <nbjo460@gmail.com>
2025-06-15 17:40:32 +0300

    Factory sensor

---
+++ Factory/FactorySensor.cs
@@ -0,0 +1,18 @@
+﻿using Sensors.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensors.Factory
+{
+    internal class FactorySensor
+    {
+        //public BaseSensor CreateSensor()
+        //{
+        //    BaseSensor sensor = new BaseSensor();
+        //    return sensor;
+        //}
+    }
+}
