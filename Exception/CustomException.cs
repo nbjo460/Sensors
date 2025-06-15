a06d8ead6546b252e3ee70e0fef6aa0638cf8a60
menachem yarhi <nbjo460@gmail.com>
2025-06-15 15:08:17 +0300

    added exception Location not in range

---
+++ Exception/CustomException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class LocationNotInRange : Exception
+{
+    int Location;
+    int MaxLocation;
+    public LocationNotInRange(int _location, int _max_location) : base($"The Location: {_location}, is not within 1 and {_max_location}")
+    {
+        Location = _location;
+        MaxLocation = _max_location;
+    }
+
+    
+}
+
+
